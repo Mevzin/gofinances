22ace570610cd3303bf47e4f7a6cf94a9239e222
Thiago Torres de Souza <thiagomev@gmail.com>
2021-11-25 07:38:15 -0300

    Adding monthy select

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components'
 
 import { HistoryCard } from '../../Components/HistoryCard';
 import {
   Container,
   Header,
   Title,
-  Content
+  Content,
+  ChartContainer
 } from './styles';
 import { categories } from '../../utils/categories';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 interface TransactionsData {
   type: 'positive' | 'negative';
@@ -21,12 +26,16 @@ interface TransactionsData {
 interface CategoryData {
   key: string;
   name: string;
-  total: string;
+  total: number;
+  totalFormatted: string;
   color: string;
+  percent: string;
 }
 
 export function Resume() {
   const [totalByCategorys, setTotalByCategorys] = useState<CategoryData[]>([])
+
+  const theme = useTheme();
 
   async function loadData() {
     const dataKey = "@gofinances:transactions";
@@ -34,7 +43,13 @@ export function Resume() {
     const responseFormatted = response ? JSON.parse(response) : [];
 
     const expensives = responseFormatted
-      .filter((expensive: TransactionsData) => expensive.type === "negative");
+    .filter((expensive: TransactionsData) => expensive.type === "negative");
+
+    const expensivesTotal = expensives.reduce((acumullator: number, expensive: TransactionsData) => {
+      return acumullator + Number(expensive.amount);
+    },0) ;     
+
+    console.log(expensivesTotal);
 
     const totalByCategory: CategoryData[] = [];
 
@@ -48,15 +63,20 @@ export function Resume() {
       });
 
       if (categorySum > 0) {
-        const total = categorySum.toLocaleString('pt-BR', {
+        const totalFormatted = categorySum.toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
         });
+
+        const percent = `${(categorySum/expensivesTotal * 100).toFixed(0)}%`;
+
         totalByCategory.push({
           key: category.key,
           name: category.name,
           color: category.color,
-          total,
+          total: categorySum,
+          totalFormatted,
+          percent
         });
       }
     });
@@ -74,17 +94,39 @@ export function Resume() {
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
-      <Content>
-      {
-        totalByCategorys.map(item => (
-          <HistoryCard
-            title={item.name}
-            amount={item.total}
-            color={item.color}
-            key={item.key}
+      <Content 
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingBottom: useBottomTabBarHeight(),
+        }}
+      >
+        <ChartContainer>
+          <VictoryPie
+            data={totalByCategorys}
+            colorScale={totalByCategorys.map(category => category.color)}
+            style={{
+              labels: {
+                fontSize: RFValue(18),
+                fontWeight: "bold",
+                fill: theme.colors.shape
+              }
+            }}
+            labelRadius={50}
+            x="percent"
+            y="total"
           />
-        ))
-      }
+        </ChartContainer>
+        {
+          totalByCategorys.map(item => (
+            <HistoryCard
+              title={item.name}
+              amount={item.totalFormatted}
+              color={item.color}
+              key={item.key}
+            />
+          ))
+        }
       </Content>
     </Container>
   )
